c43181121a222ed125b26222c7205c1ccad17202
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2015-07-12 15:48:17 -0400

    Added shooting and working custom AI script to sorceress

---
+++ Quicksilver_Project/Assets/Scripts/Enemy/DetermineTargetLocation.cs
@@ -0,0 +1,22 @@
+﻿using RAIN.Action;
+using RAIN.Core;
+using UnityEngine;
+
+[RAINAction]
+public class DetermineTargetLocation : RAINAction
+{
+	EnemyShooting enemyShoot;
+	GameObject playerTarget;
+
+	public override void Start (AI ai)
+	{
+		enemyShoot = ai.Body.GetComponent<EnemyShooting>();
+	}
+
+
+	public override ActionResult Execute(AI ai)
+	{
+
+		return ActionResult.SUCCESS;
+	}
+}
---
+++ Quicksilver_Project/Assets/Scripts/Enemy/EnemyShooting.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour 
+{
+	public Vector3 targetPosition;
+
+	LineRenderer attackLine;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		attackLine = GetComponent<LineRenderer>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+		{
+			Shoot ();
+		}
+		else
+		{
+			DisableEffects ();
+		}
+	}
+
+	public void DisableEffects()
+	{
+		attackLine.enabled = false;
+	}
+
+	void Shoot ()
+	{
+		attackLine.enabled = true;
+		attackLine.SetPosition (0, transform.position);
+		attackLine.SetPosition (1, targetPosition);
+	}
+}
