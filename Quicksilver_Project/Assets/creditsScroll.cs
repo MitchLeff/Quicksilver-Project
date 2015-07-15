322dcd6371c6efd5b5b6fd1995aa386ec97ade1b
William <wgu36@gatech.edu>
2015-07-15 00:04:03 -0400

    credits

---
+++ Quicksilver_Project/Assets/creditsScroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditsScroll : MonoBehaviour {
+
+	bool crawling = true;
+	RectTransform rt;
+	
+	// Use this for initialization
+	void Start () {
+		rt = GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!crawling)
+			return;
+		rt.Translate(Vector3.down * Time.deltaTime*20);
+		/*
+		if (gameObject.transform.position.y > .8)
+		{
+			crawling = false;
+		}
+		*/
+	}
+}
