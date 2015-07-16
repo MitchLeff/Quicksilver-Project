c37ce81fdbb8a480e6510f993d035c4e2cb15431
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2015-07-16 07:56:53 -0400

    Updated to credits to exit on input or end of credits

--- Quicksilver_Project/Assets/creditsScroll.cs
+++ Quicksilver_Project/Assets/creditsScroll.cs
@@ -1,27 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class creditsScroll : MonoBehaviour {
+public class creditsScroll : MonoBehaviour 
+{
 
 	bool crawling = true;
 	RectTransform rt;
-	
+	private GameObject gm;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		rt = GetComponent<RectTransform> ();
+		gm = GameObject.Find ("GameManager");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (!crawling)
-			return;
-		rt.Translate(Vector3.down * Time.deltaTime*20);
-		/*
-		if (gameObject.transform.position.y > .8)
+		if (Input.GetButtonDown("Start"))
 		{
-			crawling = false;
+			gm.GetComponent<GameManager>().ResumeGame();
+			StartCoroutine(gm.GetComponent<GameManager>().ChangeLevel(0));
 		}
-		*/
+
+		if (!crawling)
+		{
+			StartCoroutine(gm.GetComponent<GameManager>().ChangeLevel(0));
+		}
+		else if (crawling)
+		{
+			rt.Translate(Vector3.down * Time.deltaTime*20);
+			if (rt.anchoredPosition3D.y < -400)
+			{
+				crawling = false;
+			}
+		}
 	}
 }
