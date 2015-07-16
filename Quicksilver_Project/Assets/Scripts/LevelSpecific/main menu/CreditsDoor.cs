aac56329efb1edb62a24711b0fc266237b441561
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2015-07-15 21:03:47 -0400

    Added scripting to credits door

---
+++ Quicksilver_Project/Assets/Scripts/LevelSpecific/main menu/CreditsDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsDoor : MonoBehaviour 
+{
+	private GameObject gm;
+	// Use this for initialization
+	void Start () 
+	{
+		gm = GameObject.Find ("GameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		GameObject door = GameObject.FindGameObjectWithTag("Credits");
+		door.GetComponent<Animation>().Play("open");
+		StartCoroutine(gm.GetComponent<GameManager>().ChangeLevel(2));
+	}
+
+}
