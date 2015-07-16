ba852678721ee15ab23c0913a956b479f9164b05
zeroxiii <rlodhia@lawn-143-215-118-51.lawn.gatech.edu>
2015-07-16 14:39:51 -0400

    Updated buttons

---
+++ Quicksilver_Project/Assets/Scripts/Managers/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ButtonManager : MonoBehaviour, IPointerEnterHandler 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//		if (EventSystem.current.currentSelectedGameObject != this.gameObject)
+//			gameObject.GetComponent<Button>().
+	}
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		Debug.Log ("Mouse over button");
+		EventSystem.current.SetSelectedGameObject(this.gameObject);
+	}
+
+}
