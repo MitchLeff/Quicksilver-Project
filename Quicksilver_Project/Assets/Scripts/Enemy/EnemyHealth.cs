f534e096f44147be4d0d37e8c8bc86905742d3e7
zeroxiii <rlodhia@Rahmaan-Lodhias-Macbook.local>
2015-07-16 22:34:20 -0400

    Updated brute to take damage and display second particle effect

---
+++ Quicksilver_Project/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class EnemyHealth : MonoBehaviour 
+{
+
+	public int Health = 10;
+
+	private Animator anim;
+	private AIRig ai;
+	private int startingHealth;
+	private CapsuleCollider col;
+	public GameObject DamageSmokePS;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		ai = GetComponentInChildren<AIRig>();
+		col = GetComponent<CapsuleCollider>();
+		startingHealth = Health;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ParticleSystem ps = DamageSmokePS.GetComponent<ParticleSystem>();
+
+		if (Health <= startingHealth/2 && !ps.isPlaying)
+		{
+			ps.Play();
+		}
+
+		if (Health <= 0)
+		{
+			Die ();
+		}
+
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("TurnOffAnimator"))
+		{
+			anim.enabled = false;
+		}
+	}
+
+	public void TakeDamage (int amount)
+	{
+		Debug.Log ("Take Damage");
+		Health = Health - amount;
+		anim.SetFloat("Speed", 0f);
+		anim.SetTrigger("HitTrigger");
+		//ai.enabled = false;
+	}
+
+	public void Die ()
+	{
+		anim.SetBool("Death", true);
+		ai.enabled = false;
+		col.enabled = false;
+		DamageSmokePS.transform.position = this.transform.position;
+	}
+}
---
+++ Quicksilver_Project/Assets/Scripts/Player/RobotProjectileManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotProjectileManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnParticleCollision (GameObject other)
+	{
+		if (other.tag == "Enemy")
+		{
+			Debug.Log ("Enemy Hit");
+			other.GetComponent<EnemyHealth>().TakeDamage(5);
+		}
+	}
+}
