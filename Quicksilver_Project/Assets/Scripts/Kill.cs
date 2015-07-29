bbe259de8400b76fce55bda6788e4bc14e2f9b46
Larry He <larry.he@gatech.edu>
2015-07-29 16:46:03 -0400

    Level 3 80% done. Added kill prefab.

---
+++ Quicksilver_Project/Assets/Scripts/Kill.cs
@@ -0,0 +1,14 @@
+﻿//Larry He
+
+using UnityEngine;
+using System.Collections;
+
+public class Kill : MonoBehaviour 
+{	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player")
+		{
+			collision.gameObject.GetComponent<RobotEnergy>().TakeDamage (100);
+		}
+	}
+}
